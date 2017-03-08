500ad3d6c86ebd7b629a4ef128bfb36df57fe1ff
Hata <WoodyAllenZombieJesus@yandex.ru>
2017-03-08 22:28:30 +0300

    Конец

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 int kth_bit(int k)
 {
-    return 1 << k;
+    return 1 << (k-1);
 }
 
 int ex2(int n)
@@ -24,58 +24,70 @@ int ex2(int n)
     return r;
 }
 
-int main(void)
+int main(int argc, char** argv)
 {
-    int n,k;
+    int n,k,threads;
     complexd *a,*b,u[4];
-    cin >> n >> k;
-    int length = kth_bit(n);
+    long long int length;
+    unsigned seed;
+    float arg;
+
+    if (argc < 3)
+    {
+        cout << "Недостаточно аргументов";
+        return -1;
+    }
+    n = atoi(argv[1]);
+    k = atoi(argv[2]);
+    threads = atoi(argv[3]);
+
     u[0] = u[1] = u[2] = sqrt(2)/2;
     u[3] = -sqrt(2)/2;
-    srand(time(NULL));
 
-    cout << "length:" << length << endl;
+    length = round(pow(2,n));
     a = new complexd [length];
     b = new complexd [length];
-    #pragma omp parallel shared(a)
+
+    #pragma omp parallel num_threads(threads) private(seed,arg) shared(a)
     {
-        unsigned seed = omp_get_thread_num()+time(NULL);
-        cout << omp_get_thread_num() << "th thread" << endl;
+        seed = omp_get_thread_num()+time(NULL);
         #pragma omp for
         for (int i = 0; i < length; i++)
         {
-            a[i] = complex <float> ((float)(rand_r(&seed)%201)/100-1,(float)(rand_r(&seed)%201)/100-1);
+            arg = (float)rand_r(&seed);
+            a[i] = complex <float> (cos(arg),sin(arg));
         }
-    }
-    for (int i = 0; i < length; i++)
-    {
-        cout << a[i];
     }
 
     double start=omp_get_wtime();
 
-    #pragma omp parallel shared(a,b)
+    #pragma omp parallel for shared(a,b)
     for (int i = 0; i < length; i++)
     {
         if (kth_bit(k) & i == 0)
             b[i]=u[0]*a[i]+u[1]*a[i xor kth_bit(k)];
         else
             b[i]=u[2]*a[i xor kth_bit(k)]+u[3]*a[i];
-}
+    }
 
-double finish=omp_get_wtime();
+    double finish=omp_get_wtime();
 
-for (int i = 0; i < length; i++)
-{
-    cout << b[i] << " ";
-}
+    // for (int i = 0; i < length; i++)
+    // {
+    //     cout << a[i] << " ";
+    // }
+    cout << endl;
+    for (int i = 0; i < length; i++)
+    {
+        cout << b[i] << " ";
+    }
 
-delete []a;
-delete []b;
+    delete []a;
+    delete []b;
 
-FILE *f = fopen("./data.txt", "a");
-fprintf(f,"%d %f\n",omp_get_num_procs(),finish-start);
-fclose(f);
+    FILE *f = fopen("./data.txt", "a");
+    fprintf(f,"%d %d %d %f\n",threads,n,k,finish-start);
+    fclose(f);
 
-return 0;
+    return 0;
 }
